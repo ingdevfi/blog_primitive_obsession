5dd1bf4764cb3c608d7bc6b82ed95096d391997e
ingdevfi <ingdevfi@gmail.com>
2017-03-13 23:17:22 +0100

    operator

---
+++ C#/PrimitiveObsession/PrimitiveObsession/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimitiveObsession
+{
+    class Program
+    {
+        // Ne compile pas
+        //private const Quantity = 1000;
+        private const int MAX_QUANTITY = 1000000;
+
+        static void Main(string[] args)
+        {
+            var qty = 1000;
+
+            if (qty > MAX_QUANTITY)
+                throw new Exception("qty is greater than max qty");
+        }
+    }
+}
---
+++ C#/PrimitiveObsession/PrimitiveObsession/Quantity.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimitiveObsession
+{
+    class Quantity
+    {
+        private readonly int _value;
+
+        public Quantity(int value)
+        {
+            _value = value;
+        }
+
+        public static implicit operator Quantity(int value)
+        {
+            return new Quantity(value);
+        }
+        
+        public static bool operator ==(Quantity one, Quantity other) { return one._value == other._value; }
+        public static bool operator !=(Quantity one, Quantity other) { return !(one == other); }
+        public static bool operator <(Quantity one, Quantity other) { return one._value < other._value; }
+        public static bool operator <=(Quantity one, Quantity other) { return one == other ? true : one < other; }
+        public static bool operator >(Quantity one, Quantity other) { return !(one <= other); }
+        public static bool operator >=(Quantity one, Quantity other) { return !(one < other); }
+
+        public override int GetHashCode()
+        {
+            return _value;
+        }
+
+        public override bool Equals(object obj)
+        {
+            var other = obj as Quantity;
+            if (other == null) return false;
+
+            return this == other;
+        }
+
+        //inutile
+        //public static bool operator ==(Quantity one, int other) { return one._value == other; }
+        //public static bool operator !=(Quantity one, int other) { return !(one == other); }
+        //public static bool operator <(Quantity one, int other) { return one._value < other; }
+        //public static bool operator <=(Quantity one, int other) { return one == other ? true: one < other;}
+        //public static bool operator >(Quantity one, int other) { return !(one <= other); }
+        //public static bool operator >=(Quantity one, int other) { return !(one < other); }
+
+        //public static bool operator ==(int one, Quantity other) { return other == one; }
+        //public static bool operator !=(int one, Quantity other) { return !(one == other); }
+        //public static bool operator <(int one, Quantity other) { return other > one; }
+        //public static bool operator <=(int one, Quantity other) { return other >= one; }
+        //public static bool operator >(int one, Quantity other) { return other < one; }
+        //public static bool operator >=(int one, Quantity other) { return other <= one; }
+
+        // Ne compile pas car int n'est pas un type reference
+        //public static implicit operator int( Quantity qty)
+        //{
+        //    return _value;
+        //}
+    }
+}
